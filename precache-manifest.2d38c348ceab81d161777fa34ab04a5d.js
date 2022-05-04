91abbc83fec9fdc93ffd2c2768a9a7af0356acc1
abdulsalam alkhateeb <abdulsalam@matched.se>
2022-05-04 03:39:43 +0300

    first commit

---
+++ precache-manifest.2d38c348ceab81d161777fa34ab04a5d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5282d3d765244d5724763cdb9062564f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99e4b106020f5a35ae93",
+    "url": "/static/css/3.56619fb5.chunk.css"
+  },
+  {
+    "revision": "aeecfb82b22f7bf3064b",
+    "url": "/static/css/4.a9284ac8.chunk.css"
+  },
+  {
+    "revision": "76b39b21bd5594734b06",
+    "url": "/static/css/main.0076004d.chunk.css"
+  },
+  {
+    "revision": "d0abbe2058e6111c6f24",
+    "url": "/static/js/2.796b0158.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.796b0158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99e4b106020f5a35ae93",
+    "url": "/static/js/3.b0cae5f3.chunk.js"
+  },
+  {
+    "revision": "aeecfb82b22f7bf3064b",
+    "url": "/static/js/4.b97d0af8.chunk.js"
+  },
+  {
+    "revision": "76b39b21bd5594734b06",
+    "url": "/static/js/main.f449a562.chunk.js"
+  },
+  {
+    "revision": "b12ef0e7fba4675e4aa6",
+    "url": "/static/js/runtime-main.b50dae6a.js"
+  },
+  {
+    "revision": "b4cd24a82304cd815f570aa1fc91a572",
+    "url": "/static/media/1st.b4cd24a8.png"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "b557a83bb81d0c5495b3b2d11a55dfd1",
+    "url": "/static/media/javascript.b557a83b.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "69c9b7a4e69d55a0d7ab7da1453400ef",
+    "url": "/static/media/matched.69c9b7a4.png"
+  },
+  {
+    "revision": "a124cea7ac238ebf8c82c60a6993ad15",
+    "url": "/static/media/nefero.a124cea7.png"
+  },
+  {
+    "revision": "896bba5033df8e419a18ec5cea4127e9",
+    "url": "/static/media/notification.896bba50.png"
+  },
+  {
+    "revision": "532a0baaf42615dcfe1fcd39cce12ac7",
+    "url": "/static/media/python.532a0baa.png"
+  },
+  {
+    "revision": "26b82fdc7e767aacc93b2d5c4c2f49f7",
+    "url": "/static/media/rest-api.26b82fdc.png"
+  },
+  {
+    "revision": "030d8a40c06869877b28c4d61be5de9e",
+    "url": "/static/media/sanofa.030d8a40.png"
+  },
+  {
+    "revision": "adfef8a655c738b61090bf7d5b0d8ae4",
+    "url": "/static/media/sh.adfef8a6.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  },
+  {
+    "revision": "23650a951ea7bc62118ae4ee277c3e61",
+    "url": "/static/media/university-of-aleppo-logo-syria.23650a95.jpg"
+  },
+  {
+    "revision": "225bc5e3cab86270479b7000d9a866e4",
+    "url": "/static/media/zahraa.225bc5e3.jpg"
+  }
+]);
